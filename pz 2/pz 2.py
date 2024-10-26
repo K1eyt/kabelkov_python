b232a5e0f50c41fb81f75a2e2a3a27318025bf94
K1eyt <kabelkov7.ru@gmail.com>
2024-10-26 17:00:15 +0300

    залил pz_2

--- pz 2/pz 2.py
+++ pz 2/pz 2.py
@@ -1,11 +1,14 @@
 #В12. Дано двузначное число. Найти сумму и произведение его цифр.
-number = int(input("Введите двузначное число: "))
-if 10 <= number <= 99:  #Проверка, двузначное ли число
-    x = number // 10
-    y = number % 10
-    sum = x + y
-    proiz = x * y
-    print(f"Сумма цифр: {sum}")
-    print(f"Произведение цифр: {proiz}")
-else:
+try:
+    number = int(input("Введите двузначное число: "))
+    if 10 <= number <= 99:  #Проверка, двузначное ли число
+        x = number // 10
+        y = number % 10
+        sum = x + y
+        proiz = x * y
+        print(f"Сумма цифр: {sum}")
+        print(f"Произведение цифр: {proiz}")
+    else:
+        print("Ошибка: переписывай")
+except ValueError:
     print("Ошибка: переписывай")
