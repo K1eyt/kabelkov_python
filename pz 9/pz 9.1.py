6a8b3b745bfdbeb1b0d2112d1ab4fa93321ea4bd
K1eyt <kabelkov7.ru@gmail.com>
2025-04-11 23:28:14 +0300

    pz 9 10

--- pz 9/pz 9.1.py
+++ pz 9/pz 9.1.py
@@ -4,15 +4,15 @@ Words = {}
 inf = 'one two three four five six seven eight nine ten'
 inf = inf.split()
 
-Words['Один'] = inf[0]
-Words['Два'] = inf[1]
-Words['Три'] = inf[2]
-Words['Четыре'] = inf[3]
-Words['Пять'] = inf[4]
-Words['Шесть'] = inf[5]
-Words['Семь'] = inf[6]
-Words['Восемь'] = inf[7]
-Words['Девять'] = inf[8]
-Words['Десять'] = inf[9]
+Words[inf[0]] = 'Один'
+Words[inf[1]] = 'Два'
+Words[inf[2]] = 'Три'
+Words[inf[3]] = 'Четыре'
+Words[inf[4]] = 'Пять'
+Words[inf[5]] = 'Шесть'
+Words[inf[6]] = 'Семь'
+Words[inf[7]] = 'Восемь'
+Words[inf[8]] = 'Девять'
+Words[inf[9]] = 'Десять'
 
 print (Words)
