d147187f0d32f725522024feca6177cac8cc65bf
Sergey <kabelkov7.ru@gmail.com>
2025-04-11 22:31:55 +0300

    пз 7

---
+++ pz 9/pz 9.1.py
@@ -0,0 +1,18 @@
+#Организовать словарь на 10 англо-русских слов, обеспечивающий
+#"перевод" английского слова на русский
+Words = {}
+inf = 'one two three four five six seven eight nine ten'
+inf = inf.split()
+
+Words['Один'] = inf[0]
+Words['Два'] = inf[1]
+Words['Три'] = inf[2]
+Words['Четыре'] = inf[3]
+Words['Пять'] = inf[4]
+Words['Шесть'] = inf[5]
+Words['Семь'] = inf[6]
+Words['Восемь'] = inf[7]
+Words['Девять'] = inf[8]
+Words['Десять'] = inf[9]
+
+print (Words)
